bcf9f0a3c9cbf220ed8c06dc52e083b861f3eb94
Henok Addis <henok4@gmail.com>
2014-03-22 17:08:20 -0400

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,19 @@ describe User do
   it { should respond_to(:password_confirmation) }
     it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
 
   it { should be_valid }
@@ -107,8 +120,8 @@ describe "with a password that's too short" do
       specify { expect(user_for_invalid_password).to be_false }
     end
     describe "remember token" do
-      before { @user.save }
-      its(:remember_token) { should_not be_blank }
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
   end
 end
