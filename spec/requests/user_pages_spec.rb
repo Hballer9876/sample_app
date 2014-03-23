24cd3cfbfa35e835b9b073d055173b895727955b
Henok Addis <henok4@gmail.com>
2014-03-23 02:45:04 -0400

    add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,30 +29,25 @@ describe "User pages" do
     end
   end
   describe "delete links" do
-
-      it { should_not have_link('delete') }
-
-      describe "as an admin user" do
-        let(:admin) { FactoryGirl.create(:admin) }
-        before do
-          sign_in admin
-          visit users_path
-        end
-
-        it { should have_link('delete', href: user_path(User.first)) }
-        it "should be able to delete another user" do
-          expect do
-            click_link('delete', match: :first)
-          end.to change(User, :count).by(-1)
-        end
-        it { should_not have_link('delete', href: user_path(admin)) }
+    it { should_not have_link('delete') }
+    describe "as an admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      before do
+        sign_in admin
+        visit users_path
       end
+      it { should have_link('delete', href: user_path(User.first)) }
+      it "should be able to delete another user" do
+        expect do
+          click_link('delete', match: :first)
+        end.to change(User, :count).by(-1)
+      end
+      it { should_not have_link('delete', href: user_path(admin)) }
     end
   end
-   describe "edit" do
+  describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit edit_user_path(user) }
-
     describe "page" do
       it { should have_content("Update your profile") }
       it { should have_title(full_title("Edit user")) }
@@ -68,7 +63,6 @@ describe "User pages" do
         fill_in "Confirm Password", with: user.password
         click_button "Save changes"
       end
-
       it { should have_title(new_name) }
       it { should have_selector('div.alert.alert-success') }
       it { should have_link('Sign out', href: signout_path) }
@@ -77,21 +71,17 @@ describe "User pages" do
     end
     describe "with invalid information" do
       before { click_button "Save changes" }
-
       it { should have_content('error') }
     end
   end
-
   describe "signup page" do
     before { visit signup_path }
-        let(:submit) { "Create my account" }
-
-	 describe "with invalid information" do
+    let(:submit) { "Create my account" }
+	  describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
-
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -99,29 +89,33 @@ describe "User pages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
-        
-        describe "after saving the user" do
-          before { click_button submit }
-          let(:user) { User.find_by(email: 'user@example.com') }
-
-          it { should have_link('Sign out') }
-          it { should have_title(user.name) }
-          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      
-        end
-
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-  end
+    end
   end
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
-  it { should have_content(user.name) }
-  it { should have_title(user.name) }
-end
+    before { visit user_path(user) }
 
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
 
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end
+  
 end
